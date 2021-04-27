01eb7df86440dc2e7fd3d20d819cd37fb6282744
PatrickBoynton <jpboynton3@gmail.com>
2021-04-27 10:28:07 -0400

    Fixed a typo.

---
+++ API/Data/Migrations/20210427142619_FixedSpelling.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class FixedSpelling : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsCompleteØ",
+                table: "Todos",
+                newName: "IsCompleted");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsCompleted",
+                table: "Todos",
+                newName: "IsCompleteØ");
+        }
+    }
+}
